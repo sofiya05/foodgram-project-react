e0023c7f129f0a31ff2e7f28d45e266a9051f8e0
sofiya <118591968+sofiya05@users.noreply.github.com>
2024-01-27 18:33:14 +0300

    Правки по ревью

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,4 @@
 from django.db.models import Sum
-
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet as UVS
