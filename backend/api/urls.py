a43446271ed97cda347477c6c9febfffbb060ba5
sofiya <118591968+sofiya05@users.noreply.github.com>
2024-01-30 02:13:27 +0300

    Правки по ревью

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,25 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls import url
 
 from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
 
 app_name = 'api'
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Foodgram API",
+        default_version='v1',
+        description="Документация для приложения api проекта Foodgram",
+        contact=openapi.Contact(email="dev@yandex.ru"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 router = routers.DefaultRouter()
 router.register(r'tags', TagViewSet, basename='tags')
@@ -12,4 +28,9 @@ router.register(r'recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
     path('', include(router.urls)),
+    url(
+        r'^docs/$',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc',
+    ),
 ]
